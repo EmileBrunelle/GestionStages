b23497b00a1710887697ed74ec42622b59f67a45
Werner Burat <werner.burat@gmail.com>
2018-11-01 13:43:55 -0400

    Ajout de la fonction notifyAll() dans la classe EmailsController.php servant à notifier tous les étudiants sur la plateforme lorsqu'un nouveau stage est ajouté.
    #TODO:
    Configurer le serveur courriel

---
+++ src/Controller/EmailsController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controller;
+use App\Controller\AppController;
+use Cake\Mailer\Email;
+use Cake\ORM\TableRegistry;
+
+class EmailsController extends AppController{
+
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+        if (in_array($action, ['notifyAll'])) {
+             return true;
+        }
+    }
+
+    public function notifyAll(){
+        //Identifiant du nouveau stage en URL
+        $id = $this->request->getQuery('id');
+
+        //Obtenir les adresses emails de tous les étudiants en Query
+        $query = TableRegistry::get('Students')->find();
+
+        //Création du lien envoyé par courriel:
+        $link = "http://". $_SERVER['HTTP_HOST'].$this->request->webroot."internships/view/" . $id;
+
+        //Création du titre du message envoyé par courriel:
+        $titre = 'Stage 123.ca - Ajout d\'un nouveau stage';
+
+        //Création du message envoyé par courriel:
+        $message = 'Un nouveau stage a été ajouté, accédez à ce lien pour voir le stage: ' . $link;
+
+
+        //Envoyer un courriel à chaque étudiant sur la plateforme:
+        foreach ($query as $student) {
+            $email = new Email('default');
+            $emailaddress = $student->email;
+
+            if ($emailaddress){
+                $email->to($emailaddress)->subject($titre)->send($message);
+            }
+        }
+
+        return $this->redirect(['controller' => 'internships', 'action' => 'index']);
+    }
+}
+?>
